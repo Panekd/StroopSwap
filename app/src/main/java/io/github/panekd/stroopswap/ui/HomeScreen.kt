1d3244d11954d67ca41133e45fb23df9f12b6fad
Rose <88516521+Panekd@users.noreply.github.com>
2026-04-23 12:55:48 +0100

    Change settings and info buttons to IconButtons

--- app/src/main/java/io/github/panekd/stroopswap/ui/HomeScreen.kt
+++ app/src/main/java/io/github/panekd/stroopswap/ui/HomeScreen.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import io.github.panekd.stroopswap.R
 
@@ -15,8 +18,18 @@ fun HomeScreen(toSettings: () -> Unit, toInfo: () -> Unit, toGame: () -> Unit) {
     Column {
         Row {
             Spacer(Modifier.weight(1f))
-            Button(onClick = { toSettings() }) { Text("Settings") }
-            Button(onClick = { toInfo() }) { Text("Info") }
+            IconButton(onClick = { toSettings() }) {
+                Icon(
+                    painter = painterResource(R.drawable.settings_24px),
+                    contentDescription = "Settings button"
+                )
+            }
+            IconButton(onClick = { toInfo() }) {
+                Icon(
+                    painter = painterResource(R.drawable.info_24px),
+                    contentDescription = "Info button"
+                )
+            }
         }
         Text(stringResource(R.string.app_name))
         Button(onClick = { toGame() }) { Text("Play") }
