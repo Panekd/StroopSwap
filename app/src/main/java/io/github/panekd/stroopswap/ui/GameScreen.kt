3aaae86fbb19cd59a6d3c4cd7ba33314ab80997a
Rose <88516521+Panekd@users.noreply.github.com>
2026-04-23 14:38:57 +0100

    Added stuff to the game screen

--- app/src/main/java/io/github/panekd/stroopswap/ui/GameScreen.kt
+++ app/src/main/java/io/github/panekd/stroopswap/ui/GameScreen.kt
@@ -1,16 +1,59 @@
 package io.github.panekd.stroopswap.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import io.github.panekd.stroopswap.R
+import io.github.panekd.stroopswap.ui.theme.Red
+
+enum class Modes {
+    Colour,
+    Word
+}
 
 @Composable
 fun GameScreen() {
     var score by remember { mutableIntStateOf(0) }
+    var mode by remember { mutableStateOf(Modes.Colour) }
 
-    Text(score.toString())
+    Column {
+        IconButton({ pause() }) {
+            Icon(
+                painter = painterResource(R.drawable.pause_24px),
+                contentDescription = "Pause button"
+            )
+        }
+        Text("Score: $score")
+        Text(
+            text = "BLUE",
+            color = Red,
+            fontSize = 100.sp
+        )
+        Mode(mode)
+    }
+}
+
+@Composable
+fun Mode(mode: Modes) {
+    val text = when (mode) {
+        Modes.Colour -> "COLOUR MODE"
+        Modes.Word -> "WORD MODE"
+    }
+    Text(
+        text = text,
+        fontSize = 20.sp
+    )
+}
+
+fun pause() {
 
 }
--- app/src/main/java/io/github/panekd/stroopswap/ui/theme/Color.kt
+++ app/src/main/java/io/github/panekd/stroopswap/ui/theme/Color.kt
@@ -6,6 +6,13 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+val Red = Color(0xFFE00000)
+val Orange = Color(0xFFE09000)
+val Yellow = Color(0xFFD0D000)
+val Green = Color(0xFF00E000)
+val Blue = Color(0xFF0000FD)
+val Purple = Color(0xFFC000D0)
+
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
