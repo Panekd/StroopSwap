43e81bda9db79b5c7838fc129b5ccbebc3ab91a1
Rose <88516521+Panekd@users.noreply.github.com>
2026-05-15 20:28:04 +0100

    Colour buttons + wiki link

--- app/src/main/java/io/github/panekd/stroopswap/ui/GameScreen.kt
+++ app/src/main/java/io/github/panekd/stroopswap/ui/GameScreen.kt
@@ -1,8 +1,14 @@
 package io.github.panekd.stroopswap.ui
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,20 +16,43 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.github.panekd.stroopswap.R
+import io.github.panekd.stroopswap.ui.theme.Blue
+import io.github.panekd.stroopswap.ui.theme.Green
+import io.github.panekd.stroopswap.ui.theme.Orange
+import io.github.panekd.stroopswap.ui.theme.Purple
 import io.github.panekd.stroopswap.ui.theme.Red
+import io.github.panekd.stroopswap.ui.theme.Yellow
 
 enum class Modes {
     Colour,
     Word
 }
 
+enum class Colours (val color: Color) {
+    RED(Red),
+    ORANGE(Orange),
+    YELLOW(Yellow),
+    GREEN(Green),
+    BLUE(Blue),
+    PURPLE(Purple)
+}
+
 @Composable
-fun GameScreen() {
+fun GameScreen(onCorrect: () -> Unit = {}, onFail: () -> Unit = {}) {
+    val orientation = LocalConfiguration.current.orientation
+
     var score by remember { mutableIntStateOf(0) }
-    var mode by remember { mutableStateOf(Modes.Colour) }
+    var mode by remember { mutableStateOf(Modes.entries.random()) }
+    var currentWord by remember { mutableStateOf(Colours.entries.random()) }
+    var currentColour by remember { mutableStateOf(Colours.entries.random()) }
 
     Column {
         IconButton({ pause() }) {
@@ -33,12 +62,75 @@ fun GameScreen() {
             )
         }
         Text("Score: $score")
-        Text(
-            text = "BLUE",
-            color = Red,
-            fontSize = 100.sp
-        )
-        Mode(mode)
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Row {
+                Column {
+                    Text(
+                        text = currentWord.name,
+                        color = currentColour.color,
+                        fontSize = 80.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Mode(mode)
+                }
+                ColourButtons(onClick = { colour: Colours ->
+                    {
+                        if (colour == correctColour(mode, currentWord, currentColour)) {
+                            onCorrect()
+                            // change word + colour
+                            // increase score
+                        } else {
+                            onFail()
+                            // show end screen
+                        }
+                    }
+                })
+            }
+        } else {
+            Column{
+                Text(
+                    text = currentWord.name,
+                    color = currentColour.color,
+                    fontSize = 80.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Mode(mode)
+                ColourButtons(onClick = { colour: Colours ->
+                    {
+                        if (colour == correctColour(mode, currentWord, currentColour)) {
+                            onCorrect()
+                            // change word + colour
+                            // increase score
+                        } else {
+                            onFail()
+                            // show end screen
+                        }
+                    }
+                })
+            }
+        }
+    }
+}
+
+@Composable
+fun ColourButtons(onClick: (Colours) -> Unit) {
+    FlowRow (
+        modifier = Modifier.padding(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        maxItemsInEachRow = 2
+    ) {
+        Colours.entries.forEach { colour ->
+            OutlinedButton(onClick=({ onClick(colour) })) {
+                Text(colour.name, color=colour.color, fontSize=25.sp)
+            }
+        }
+    }
+}
+
+fun correctColour(mode: Modes, currentWord:Colours, currentColour: Colours): Colours {
+    return when (mode) {
+        Modes.Colour -> currentColour
+        Modes.Word -> currentWord
     }
 }
 
@@ -50,7 +142,7 @@ fun Mode(mode: Modes) {
     }
     Text(
         text = text,
-        fontSize = 20.sp
+        fontSize = 25.sp
     )
 }
 
--- app/src/main/java/io/github/panekd/stroopswap/ui/InfoScreen.kt
+++ app/src/main/java/io/github/panekd/stroopswap/ui/InfoScreen.kt
@@ -1,9 +1,37 @@
 package io.github.panekd.stroopswap.ui
 
+import android.content.Intent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withLink
+import androidx.core.net.toUri
 
 @Composable
 fun InfoScreen() {
-    Text("Stroop test blah blah blah")
+    val context = LocalContext.current
+
+    Column {
+        Text("Stroop test blah blah blah")
+        Text(
+            buildAnnotatedString {
+                withLink(
+                    LinkAnnotation.Clickable(
+                        tag = "wikilink",
+                        linkInteractionListener = {
+                            val intent = Intent(Intent.ACTION_VIEW).apply {
+                                data = "https://en.wikipedia.org/wiki/Stroop_effect".toUri()
+                            }
+                            context.startActivity(intent)
+                        }
+                    )
+                ) {
+                    append("See Stroop effect on Wikipedia")
+                }
+            }
+        )
+    }
 }
